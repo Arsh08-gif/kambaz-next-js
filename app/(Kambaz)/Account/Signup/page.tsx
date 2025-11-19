81341b32267a97aa724e32b865e2b1c1a9072f82
Arshiya Zakir Hussain <arshiya@Arshiyas-MacBook-Air.local>
2025-11-19 14:22:59 -0500

    changed signup

--- app/(Kambaz)/Account/Signup/page.tsx
+++ app/(Kambaz)/Account/Signup/page.tsx
@@ -25,18 +25,13 @@ export default function Signup() {
     const router = useRouter();
 
     const handleSignup = async () => {
-        try {
-            console.log("inside signup");
-            console.log("user " + JSON.stringify(user));
-            const currentUser = await client.signup(user);
-            console.log("current user signedup" + JSON.stringify(currentUser));
+        console.log("inside signup");
+        console.log("user " + JSON.stringify(user));
+        const currentUser = await client.signup(user);
+        console.log("current user signedup" + JSON.stringify(currentUser));
 
-            dispatch(setCurrentUser(currentUser));
-            redirect("/Account/Profile");
-        }
-        catch(err:any){
-            alert(JSON.stringify(err.response?.data))
-        }
+        dispatch(setCurrentUser(currentUser));
+        redirect("/Account/Profile");
     };
     return (
         <div id="wd-signup-screen">
